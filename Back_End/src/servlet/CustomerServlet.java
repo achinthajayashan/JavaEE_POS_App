6b3c9eb845f7e6ba7c7ceed52006b828f30a52a9
achinthajayashan <achintha.uns.2018@gmail.com>
2023-08-24 17:01:56 +0530

    customer getall and save

--- Back_End/src/servlet/CustomerServlet.java
+++ Back_End/src/servlet/CustomerServlet.java
@@ -1,21 +1,103 @@
 package servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.sql.*;
+@WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos_app", "root", "12345678");
+            PreparedStatement pstm = connection.prepareStatement("select * from customer");
+            ResultSet rst = pstm.executeQuery();
+            PrintWriter writer = resp.getWriter();
+            resp.addHeader("Access-Control-Allow-Origin","*");
+            resp.addHeader("Content-Type","application/json");
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+
+
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                String contact = String.valueOf(rst.getInt(4));
+
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+
+                customer.add("id",id);
+                customer.add("name",name);
+                customer.add("address",address);
+                customer.add("contact",contact);
+
+                allCustomers.add(customer.build());
+            }
+
+            writer.print(allCustomers.build());
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String cusID = req.getParameter("cusID");
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
+        String cusContact = req.getParameter("cusContact");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos_app", "root", "12345678");
+
+            PreparedStatement pstm = connection.prepareStatement("insert into customer values(?,?,?,?)");
+            pstm.setObject(1, cusID);
+            pstm.setObject(2, cusName);
+            pstm.setObject(3, cusAddress);
+            pstm.setObject(4, cusContact);
+            resp.addHeader("Access-Control-Allow-Origin","*");
+            resp.addHeader("Content-Type", "application/json");
+
+            if (pstm.executeUpdate() > 0) {
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("state", "Ok");
+                response.add("message", "Successfully Added.!");
+                response.add("data", "");
+                resp.getWriter().print(response.build());
+            }
+
+        } catch (ClassNotFoundException e) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("state", "Error");
+            response.add("message", e.getMessage());
+            response.add("data", "");
+            resp.setStatus(400);
+            resp.getWriter().print(response.build());
+
+        } catch (SQLException e) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("state", "Error");
+            response.add("message", e.getMessage());
+            response.add("data", "");
+            resp.setStatus(400);
+            resp.getWriter().print(response.build());
+
+        }
     }
 
     @Override
